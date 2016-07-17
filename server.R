5460af74e777376184a3ea94097b589705ce84cd
AvirupSenGupta <aavirup@yahoo.com>
2016-07-16 23:45:47 -0400

    Update server.R

--- server.R
+++ server.R
@@ -2,14 +2,14 @@ source("./Probfunction.R")
 
 shinyServer(function(input, output) {
         output$input <- renderText({
-                
-                paste0("Your car's displacements and weight are", "<strong>", input$disp, " cu. in.", " & ", input$wt, " lb, respectively", "</strong>")
+                paste0("displacement and weight of your car are ", "<strong>", input$disp, " cu. in. & ", input$wt, " lb, respectively")
         })
-        
         output$result <- renderText({
-                probval = Probfunction(disp = input$disp, wt = input$wt) 
-     
-                paste0("The predicted probability that your car is a V-engine is", "<code>", round(probval, 2), "</code>")
+	data(mtcars)
+	model = glm(formula= vs ~ wt + disp, data=mtcars, family=binomial)
+	newdata = data.frame(wt = input$wt, disp = input$disp)
+	prob = predict(model, newdata, type="response")
+                paste0("The predicted probability that your car is a V-engine is ", "<code>", round(prob, 2), "</code>")
         })
                 
 })
