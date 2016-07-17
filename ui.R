c4bb47b8af29eb209ebea70f8e9ec38398885b24
AvirupSenGupta <aavirup@yahoo.com>
2016-07-16 23:46:18 -0400

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,20 +1,19 @@
 shinyUI(fluidPage(
         titlePanel("Claculate the probablity of your car's Calculating Probability of engine type (V or straight) "),
-        
         sidebarLayout(sidebarPanel(
-                
-                helpText("Estimate the probability of a vehicle having a `V-engine` or a `straight engine` using the a weight (1000 lbs) and engine displacement values (cu. inches)"),
+     
+                helpText("Estimate the probability of a vehicle having a V-engine or straight engine using the a weight (1000 lbs) and engine displacement values (cu. inches)"),
         
                 numericInput(
                         inputId  = "disp",
-                        label    = "displacement values: ",
-				   value = 180
+                        label    = strong("displacement values: "),
+				                value = 180
                 ),
                 numericInput(
                         inputId = "wt",
                         label = strong("weight (1000 lb)"),
                         value = 2.1
-                ),
+                )
         ),
         
         mainPanel(
